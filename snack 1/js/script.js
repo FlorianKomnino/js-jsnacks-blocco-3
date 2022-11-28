a8d465ea3c487c2e8f8c1a5bc6adaa66691828a8
FlorianKomnino <florian.komnino@outlook.it>
2022-11-28 16:31:31 +0100

    more elegant version

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -3,6 +3,9 @@ console.log('Hello World!');
 // ^ Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 
 let requestedNumber = parseInt(prompt('Inserisci il numero corretto'));
-while (requestedNumber !== 3) {
+
+const correctNumber = 3;
+
+while (requestedNumber !== correctNumber) {
     requestedNumber = parseInt(prompt('Inserisci il numero corretto'));
 };
