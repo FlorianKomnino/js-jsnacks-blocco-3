a5326d8cc400caf896500d7b31226392c232d0f6
FlorianKomnino <florian.komnino@outlook.it>
2022-11-28 18:01:48 +0100

    setup snack 4

---
+++ snack 4/js/script.js
@@ -0,0 +1,3 @@
+console.log('Hello World!');
+
+// ^ Prendere due array della stessa lunghezza e generarne un terzo, prendendo alternativamente gli elementi da uno e dall'altro es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
