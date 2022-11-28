0d5b55f8b6242f9fa4f03452a528f8b9d6c5bbc0
FlorianKomnino <florian.komnino@outlook.it>
2022-11-28 16:49:22 +0100

    completed snack 2

--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -1,2 +1,25 @@
 console.log('Hello World!');
 
+// ^ Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+let numbersArray = [];
+
+let numbersSum = 0;
+
+do {
+    requestedNumber = parseInt(prompt('Inserisci il numero corretto'));
+    numbersArray.push(requestedNumber);
+    numbersSum += requestedNumber;
+    console.log(numbersSum);
+} while ( numbersSum < 50 ) 
+
+
+
+
+/* {
+    console.log(numbersSum);
+    numbersSum += requestedNumber;
+    numbersArray.push(requestedNumber);
+    requestedNumber = parseInt(prompt('Inserisci il numero corretto'));
+};
+*/
