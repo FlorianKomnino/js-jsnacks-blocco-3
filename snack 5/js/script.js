95f6727b4619f051e220260eef3c48c5e2b7632f
FlorianKomnino <florian.komnino@outlook.it>
2022-11-29 11:59:39 +0100

    completed snack 5

--- snack 5/js/script.js
+++ snack 5/js/script.js
@@ -1,2 +1,13 @@
 console.log('Hello World!');
 
+/**
+ * ^ Crea una funzione che accetti due argomenti, e che restituisca un valore numerico randomico tra i due argomenti inclusi.
+ */
+
+function randomNumberBetweenLimits (limitMin, limitMax) {
+    const generatedNumber = Math.floor(Math.random() * (limitMax - limitMin) + limitMin)
+
+    return generatedNumber;
+}
+
+console.log(randomNumberBetweenLimits(1, 12));
