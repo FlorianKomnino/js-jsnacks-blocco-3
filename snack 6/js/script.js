1ba0a00cff1ebec37b5054f9b1674920fde10b72
FlorianKomnino <florian.komnino@outlook.it>
2022-11-29 12:38:43 +0100

    completed snack 6 with little help in class

---
+++ snack 6/js/script.js
@@ -0,0 +1,26 @@
+console.log('Hello World!');
+
+/**
+ *
+ * Crea una funzione che dato un array, calcoli la somma dei suoi elementi.
+ *
+ */
+
+let myExampleArray = [1, 2, 'uno', 'patate', 10]
+
+console.log(Number.isNaN(myExampleArray[3]))
+
+function getSumOfArrayElements (array) {
+    let arraySum = 0;
+    for ( let i = 0 ; i < array.length ; i++) {
+        let castedNumber = parseInt(myExampleArray[i]);
+        if (Number.isNaN(castedNumber)) {
+            console.log(`Ho eliminato dall'eleco l'elemento : ${myExampleArray[i]}`)
+        } else {
+            arraySum += array[i];
+        }
+    }
+    return arraySum;
+}
+
+console.log(getSumOfArrayElements(myExampleArray));
