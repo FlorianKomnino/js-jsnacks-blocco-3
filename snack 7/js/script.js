34316d0a07f3143b7ffcf2745e69a70cea6fc313
FlorianKomnino <florian.komnino@outlook.it>
2022-12-01 12:26:56 +0100

    after correction in class

--- snack 7/js/script.js
+++ snack 7/js/script.js
@@ -12,24 +12,17 @@ const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
 let generatedTenRandomNumbers = [];
 
 function randomNumberBetweenLimits (minValue, maxValue) {
-    const generatedNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue)
+    const generatedNumber = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 
     return generatedNumber;
 }
 
-const randomNumbersWanted = 10;
+while ( generatedTenRandomNumbers.length < 10 ) {
+    const randomIndex = randomNumberBetweenLimits(0, startingList.length - 1);
 
-function copyChecker (value1, value2) {
-    let checker;
-    if (value1 == value2) {
-        checker = true;
-    } else {
-        checker = false;
+    if (!generatedTenRandomNumbers.includes(startingList[randomIndex])) {
+        generatedTenRandomNumbers.push(startingList[randomIndex]);
     }
-    return checker;
 }
 
-for ( i = 0 ; i == randomNumbersWanted ; i++) {
-    let numberToCheck = randomNumberBetweenLimits(1, randomNumbersWanted);
-    if ()
-}
+console.log(generatedTenRandomNumbers);
